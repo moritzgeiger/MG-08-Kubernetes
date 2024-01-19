03e7d47653b64785de6b0654ef003f42ac46ed24
Moritz Geiger <moritzgeiger@hotmail.de>
2024-01-19 14:57:17 +0100

    first commit

---
+++ airflow/dags/test_dag.py
@@ -0,0 +1,32 @@
+from airflow.utils.dates import datetime
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.models.baseoperator import chain
+
+################### CONFIGURATION ###################
+# Set the configs name
+
+default_args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2023, 9, 19),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+with DAG(
+    dag_id='test_dag',
+    description=f'Runs the test dag',
+    default_args=default_args,
+    schedule_interval="0 3 1 * *", # every 3 months
+    max_active_runs=1,
+    catchup=False
+    ) as dag:
+
+    start = DummyOperator(
+            task_id='start',
+            )
+
+    chain(start)
+    
